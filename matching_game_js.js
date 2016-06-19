659f745e5dea22fff1899c8068b310ff62d7b38f
Freddy Mendoza <freddy12120@gmail.com>
2016-06-19 17:05:47 -0500

    my solution

---
+++ matching_game_js.js
@@ -0,0 +1,55 @@
+
+/* 
+ Matching gamer
+ author: freddy120
+*/
+var numberOfFaces = 5;
+var theLeftSide = document.getElementById("leftSide");
+var theRightSide = document.getElementById("rightSide");
+var theBody = document.getElementsByTagName("body")[0];
+
+function generateFaces() {
+    theLeftSide.remov
+    for(var count = 0; count < numberOfFaces; count++){
+        var img = document.createElement("img");
+        img.src = "smile.png";
+        var imgTop = Math.random()*401;
+        imgTop = Math.floor(imgTop);
+        img.style.top = imgTop + "px";
+        var imgLeft = Math.random()*401;
+        imgLeft = Math.floor(imgLeft);
+        img.style.left = imgLeft + "px";
+
+        theLeftSide.appendChild(img);
+    }
+
+    var leftSideImages = theLeftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+    theRightSide.appendChild(leftSideImages);
+
+
+    theLeftSide.lastChild.onclick = 
+    function nextLevel(event) {
+        event.stopPropagation();
+        numberOfFaces += 5;
+
+        // remove all nodes from  left and right sides 
+        while(theLeftSide.firstChild)
+            theLeftSide.removeChild(theLeftSide.firstChild);
+        while(theRightSide.firstChild)
+            theRightSide.removeChild(theRightSide.firstChild);
+        
+        generateFaces();
+    };
+
+
+    theBody.onclick = function gameOver() {
+        alert("Game Over!");
+        theBody.onclick = null;
+        theLeftSide.lastChild.onclick = null; 
+    };
+
+}
+
+
+window.onload = generateFaces;
